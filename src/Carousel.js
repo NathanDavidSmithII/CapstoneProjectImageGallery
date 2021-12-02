878ff16afad4a55b661025a80f60ef19bfb7cd35
Nathan Smith <redjunkopera@gmail.com>
2021-12-02 12:49:54 -0500

    Initial commit

---
+++ src/Carousel.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <img alt="" src="https://i0.wp.com/therebelheart.net/wp-content/uploads/2018/12/vintage-christmas-ads-17.jpg?resize=429%2C600" />
+      <p className="legend">Christmas time is here</p>
+    </div>
+    <div>
+      <img alt="" src="http://cdn.shopify.com/s/files/1/0317/9853/files/b86cadcb64502c7ee29cfbb4dca81c99_14e8934e-9280-4fee-9ecd-e2fe1ba3a26a_large.jpg?5448" />
+      <p className="legend">Happiness and cheer</p>
+    </div>
+    <div>
+      <img alt="" src="https://assets.hongkiat.com/uploads/christmas-ads-of-yesteryears/warbonds.jpg" />
+      <p className="legend">Fun for all that children call</p>
+    </div>
+    <div>
+      <img alt="" src="https://metvcdn.metv.com/fo9sa-1481644937-embed-xmasads_hoover.jpg" />
+      <p className="legend">Their favorite time of year</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.pinimg.com/originals/93/cb/f1/93cbf18ad8ab4b325a86f49ed43f069f.jpg" />
+      <p className="legend">Snowflakes in the air</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.ebayimg.com/images/g/gHUAAOSwoZJdt2Tg/s-l300.jpg" />
+      <p className="legend">Carols everywhere</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.ebayimg.com/images/g/rrQAAOSwZ5VfqDrO/s-l400.jpg" />
+      <p className="legend">Olden times and ancient rhymes</p>
+    </div>
+    <div>
+      <img alt="" src="https://miro.medium.com/max/329/1*UddtMn_DcrLhyfLnYupIDA.jpeg" />
+      <p className="legend">Of love and dreams to share</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.pinimg.com/originals/ff/72/e7/ff72e7c4e4455743af4ea7821c515e1d.jpg" />
+      <p className="legend">Sleigh bells in the air</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.pinimg.com/474x/d6/10/ce/d610cece57d8b262bf839f34485d386b--soft-drink-x-rays.jpg" />
+      <p className="legend">Beauty everywhere</p>
+    </div>
+    <div>
+      <img alt="" src="https://i.pinimg.com/550x/df/55/1c/df551c39ddf1919d36ff67cac62ae69d.jpg" />
+      <p className="legend">Yuletide by the fireside</p>
+    </div>
+    <div>
+      <img alt="" src="https://carolynthomas.files.wordpress.com/2013/12/sendingsmokes.jpg?w=584" />
+      <p className="legend">And joyful memories there</p>
+    </div>
+    <div>
+      <img alt="" src="http://www.saturdayeveningpost.com/wp-content/uploads/satevepost/1958-shick-w-razor-copy1.jpg" />
+      <p className="legend">Oh that we could always see</p>
+    </div>
+    <div>
+      <img alt="" src="https://8bitbeerblog.files.wordpress.com/2012/12/schlitz-1959.jpg?w=248&h=300" />
+      <p className="legend">Such spirit in the year</p>
+    </div>
+  </Carousel>
+);
